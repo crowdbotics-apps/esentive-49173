9df0ef60a6c5abfafc3a4ff0169b51158dde374a
Crowdbotics <team@crowdbotics.com>
2024-08-27 18:53:54 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "esentive-49173",
+  url: "https://esentive-49173.botics.co",
+  api: "https://esentive-49173.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
